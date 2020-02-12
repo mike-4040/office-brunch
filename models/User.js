712f2a2569c49c70bc7011e4eec2c59e8b92e4c4
Mike Kravtsov <mike4040@me.com>
2020-02-11 21:33:45 -0800

    orm is ready

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const connection = require('./connection');
+const { userMap } = require('./maps');
+const { toJS, toSQL } = require('./jsToSqlMaps');
 
 const User = {
   all: cb =>
@@ -10,23 +12,23 @@ const User = {
       'SELECT firstName, lastName, email FROM users',
       (err, res) => {
         let reply = {};
-        if (err) reply = { code: 1, payload: 'Database error' };
+        if (err)
+          reply = { code: 1, payload: 'Database error' };
         else if (res.length === 0)
           reply = { code: 2, payload: 'No user found' };
-        else reply = { code: 0, payload: res };
+        else
+          reply = { code: 0, payload: res.map(user => toJS(user, userMap)) };
         cb(reply);
-      }
-    ),
+      }),
   auth: (email, password, cb) => {
-    const query =
-      'SELECT id, firstName, lastName, password FROM Users WHERE email = ? LIMIT 1';
+    const query = 'SELECT id, firstName, lastName, password FROM Users WHERE email = ? LIMIT 1';
     connection.query(query, [email], (err, res) => {
       let reply = {};
       if (err) {
         reply = { code: 1, message: 'Database error' };
         return cb(reply);
       }
-      const user = res[0];
+      const user = toJS(res[0], userMap);
       if (!user) reply = { code: 2, message: 'User not found' };
       else if (bcrypt.compareSync(password, user.password)) {
         const token = jwt.sign(
@@ -41,37 +43,27 @@ const User = {
   },
   create: (inUser, cb) => {
     const user = { ...inUser };
-    user.password = bcrypt.hashSync(
-      user.password,
-      bcrypt.genSaltSync(10),
-      null
-    );
-    const query =
-      'INSERT INTO Users ( ??, ??, ??, ??, ??) VALUES( ?, ?, ?, ?, ?)';
-    connection.query(
-      query,
-      [...Object.keys(user), ...Object.values(user)],
-      (err, res) => {
-        console.log('ERR', err, 'RES', res);
-        if (err) return cb({ code: 1, message: err.sqlMessage });
-        cb({ code: 0, message: res.insertId });
-      }
-    );
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    const dbUser = toSQL(user, userMap);
+    const query = 'INSERT INTO Users ( ??, ??, ??, ??, ??) VALUES( ?, ?, ?, ?, ?)';
+    connection.query(query, [...Object.keys(dbUser), ...Object.values(dbUser)], (err, res) => {
+      console.log('ERR', err, 'RES', res);
+      if (err) return cb({ code: 1, message: err.sqlMessage });
+      cb({ code: 0, message: res.insertId });
+    });
   },
   findById: (id, cb) => {
     const query = 'SELECT firstName, lastName, email, CompanyId FROM users WHERE id = ?';
-    connection.query(
-      query,
-      [id],
-      (err, res) => {
-        let reply = {};
-        if (err) reply = { code: 1, message: 'Database error', payload: err };
-        else if (res.length === 0)
-          reply = { code: 2, message: 'No user found' };
-        else reply = { code: 0, payload: res };
-        cb(reply);
-      }
-    );
+    connection.query(query, [id], (err, res) => {
+      let reply = {};
+      if (err)
+        reply = { code: 1, message: 'Database error', payload: err };
+      else if (res.length === 0)
+        reply = { code: 2, message: 'No user found' };
+      else
+        reply = { code: 0, payload: toJS(res, userMap) };
+      cb(reply);
+    });
   }
 };
 
